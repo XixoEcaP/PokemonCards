e60bc7f09fd56ea9844ae79d0183e7115ff3e19b
XixoEcaP <159262212+XixoEcaP@users.noreply.github.com>
2024-05-21 23:55:11 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './style.css';  // You can keep or remove this line based on your styling preference
+import './style.css';  
 
 createApp(App).mount('#app');
